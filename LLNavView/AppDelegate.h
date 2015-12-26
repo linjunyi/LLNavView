16740639ab1cd892100be13713fda44be3ac6934
林君毅 <junyi.lin@renren-inc.com>
2015-12-26 17:49:10 +0800

    Initial Commit

---
+++ LLNavView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLNavView
+//
+//  Created by lin on 15/12/26.
+//  Copyright © 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
